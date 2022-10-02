75ce8c35a86eb2cbedce1359d6c543981f8c13c0
Bijay Sharma <yoursbijay@outlook.com>
2022-10-02 10:18:52 +0530

    Test CI

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => res.send(`BACKEND SERVER IS RUNNING ON PORT ${process.env.PORT}`));
+app.get("/", (req, res) => res.send(`BACKEND SERVER IS RUNNING`));
 
 export default app;
